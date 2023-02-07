414513f75897d0cc28365259ca4d5927ee64bdae
kiichigo <kiichigo@yahoo.co.jp>
2023-02-07 11:04:31 +0900

    pep8

--- karajibi.py
+++ karajibi.py
@@ -7,11 +7,14 @@ ZEN = "".join(chr(0xff01 + i) for i in range(94))
 HAN = "".join(chr(0x21 + i) for i in range(94))
 ZEN2HAN = str.maketrans(ZEN, HAN)
 
+
 def toHan(s):
     return s.translate(ZEN2HAN)
 
+
 def toInt(s):
     return int(re.search(r'\d+', toHan(s)).group(0))
+
 
 def getNums():
     res = requests.get("https://ssc3.doctorqube.com/karajibi/")
@@ -22,12 +25,15 @@ def getNums():
     waitmin = toInt(smpcurrent.contents[6].contents[1].string)  # 次にお取りできる順番での目安待ち時間
     return (callnum, waitnum, waitmin)
 
+
 def doit():
     callnum, waitnum, waitmin = getNums()
     print("%s, %d, %d, %d" % (datetime.now().isoformat(), callnum, waitnum, waitmin))
 
+
 def main():
     doit()
+
 
 if __name__ == "__main__":
     main()
