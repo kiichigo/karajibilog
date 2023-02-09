2409b5cb751877f21a0eaa92a4b7d14c5ac52fe9
kiichigo <kiichigo@yahoo.co.jp>
2023-02-09 16:55:51 +0900

    write to file and loop

--- karajibi.py
+++ karajibi.py
@@ -1,7 +1,9 @@
 import requests
 import re
+import os.path as p
 from datetime import datetime
-# from bs4 import BeautifulSoup
+import json
+import time
 
 ZEN = "".join(chr(0xff01 + i) for i in range(94))
 HAN = "".join(chr(0x21 + i) for i in range(94))
@@ -14,16 +16,6 @@ def toHan(s):
 
 def toInt(s):
     return int(re.search(r'\d+', toHan(s)).group(0))
-
-
-# def getNums_old():
-#     res = requests.get("https://ssc3.doctorqube.com/karajibi/")
-#     soup = BeautifulSoup(res.text)
-#     smpcurrent = soup.find(id="smpcurrent")
-#     callnum = toInt(smpcurrent.contents[1].contents[1])  # 呼出番号
-#     waitnum = toInt(smpcurrent.contents[2].contents[1])  # 待ち人数
-#     waitmin = toInt(smpcurrent.contents[6].contents[1].string)  # 次にお取りできる順番での目安待ち時間
-#     return (callnum, waitnum, waitmin)
 
 
 def getNums():
@@ -68,7 +60,7 @@ def getNums():
     f_waittime = False
     for tag_text in re.finditer(r"<(.*?)>(.*?)(?=<)", res.text):
         txt = tag_text.group(2)
-        print(tag_text)
+        #print(tag_text)
         if f_call:
             callnum = toInt(txt)  # 呼出番号
         elif f_waitnum:
@@ -89,9 +81,27 @@ def getNums():
     return (callnum, waitnum, waitmin)
 
 
+def writeFile(fn, newData):
+    datas = []
+    if p.isfile(fn):
+        datas = json.load(open(fn, 'r'))
+    if type(datas) != list:
+        datas = []
+    if not datas or datas[-1][1:] != newData[1:]:
+        datas.append(newData)
+        json.dump(datas, open(fn, 'w'))
+        return True
+    return False
+
+
 def doit():
-    callnum, waitnum, waitmin = getNums()
-    print("%s, %d, %d, %d" % (datetime.now().isoformat(), callnum, waitnum, waitmin))
+    for i in range(6 * 60):
+        callnum, waitnum, waitmin = getNums()
+        ts = datetime.now().astimezone().isoformat()
+        data = [ts, callnum, waitnum, waitmin]
+        if writeFile("karajibi.json", data):
+            print(json.dumps(data))
+        time.sleep(10)
 
 
 def main():
